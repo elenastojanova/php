16080c55d5772f0b1f61d93ab97555ac5fbe3f95
elenastojanova <elena.stojanova3528@gmail.com>
2016-02-09 21:51:18 +0100

    Class1

---
+++ c1.php
@@ -0,0 +1,22 @@
+<?php
+	echo "<p>Zdravo svetu!</p>";
+	
+	$grad = 'New York';
+	echo "Jas ziveam vo $grad";
+	echo "<br>";
+	echo "Jas ziveam vo" .$grad. "od 1994 god";
+
+	echo "<br>";
+
+	function pozdrav2(){
+		return "Zdravo";
+	}
+
+	echo "<br>";
+
+	echo pozdrav2();
+
+	
+
+
+?>

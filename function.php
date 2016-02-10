8a9b62ac39de21ddeefd875ee87888c2de2138d9
elenastojanova <elena.stojanova3528@gmail.com>
2016-02-10 18:34:25 +0100

    Functions

---
+++ function.php
@@ -0,0 +1,19 @@
+<?php
+	function years($minYears = 30){
+		echo "My age is $minYears <br>";
+	}
+
+	years(22);
+
+
+
+// function n.2
+
+	function zbir($b, $c){
+		$a = $b + $c;
+		return $a;
+	}
+
+	echo "90 + 3 = " . zbir(90, 3) . "<br>";
+?>
+
---
+++ strtoupper.php
@@ -0,0 +1,6 @@
+<?php
+	$var = '"Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that."" 
+― Martin Luther King Jr.';
+	$var = strtoupper($var);
+	echo $var;
+?>
